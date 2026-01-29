76eec0f8f51ee5378d646edea51311e75fb3b7b3
devman26173 <246410666+devman26173@users.noreply.github.com>
2026-01-29 18:18:25 +0900

    Add Prefecture Entity, Repository, dropdown

---
+++ src/main/java/com/example/join/entity/Prefecture.java
@@ -0,0 +1,23 @@
+package com.example.join.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "prefectures")
+@Entity
+public class Prefecture {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	private String Prefecture;
+	
+	public String getId() {
+		return id;
+	}
+	public String getPrefecture() {
+		return Prefecture;
+	}
+}
